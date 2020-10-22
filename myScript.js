47a09c93c1d5a57b04ebc034c16c71fd9b1f6459
Juan Saavedra <saavedra.juan.p@gmail.com>
2020-10-22 10:39:16 -0400

    functioning multi operations

--- myScript.js
+++ myScript.js
@@ -1,4 +1,10 @@
 displayValue = '';
+let firstValue;
+let runningValue;
+let firstVal;
+let secondVal;
+let oper;
+
 const evaluate = document.querySelector('.execute');
 const number = document.querySelectorAll('.number');
 const operator = document.querySelectorAll('.function');
@@ -37,6 +43,14 @@ function updateDisplay(number) {
     const display = document.querySelector('.display h2');
     display.textContent = number;
 }
+function evaluateButton() {
+    evaluate.addEventListener('click', function() {
+        value = operate(oper, Number(firstValue), Number(displayValue));
+        updateDisplay(value);
+    })
+    return value;
+}
+
 
 number.forEach(item => item.addEventListener('click', () => {
     displayValue += item.textContent;
@@ -45,16 +59,42 @@ number.forEach(item => item.addEventListener('click', () => {
 );
 clearBut.addEventListener('click', function() {
     displayValue = '';
+    runningValue = '';
+    firstValue = '';
+    firstVal = '';
+    secondVal = '';
     updateDisplay(displayValue);
 });
 
+evaluate.addEventListener('click', function() {
+    value = operate(oper, Number(firstVal), Number(displayValue));
+    updateDisplay(value);
+})
+
 operator.forEach(item => item.addEventListener('click', () => {
-    var firstValue = displayValue;
-    var oper = item.textContent;
-    displayValue = '';
-    evaluate.addEventListener('click', function() {
-        value = operate(oper, Number(firstValue), Number(displayValue));
-        updateDisplay(value);
-    })
+    var capturedValue = displayValue;
+    // clicking operator after a first op click
+    if (firstValue) {
+        oper = item.textContent;
+        runningValue = operate(oper, Number(firstValue), Number(displayValue));
+        updateDisplay(runningValue);
+        firstVal = runningValue;
+        secondVal = displayValue;
+        displayValue = '';
+    }
+    // clicking operator for first time
+    else {
+        if (runningValue) {
+            firstValue = runningValue;
+        }
+        else {
+            firstValue = displayValue;
+        }
+        oper = item.textContent;
+        updateDisplay(oper);
+        firstVal = firstValue;
+        secondVal = '';
+        displayValue = '';
+    }
 })
 );
